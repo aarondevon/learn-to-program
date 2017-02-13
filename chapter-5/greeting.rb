f60bf500df1ef4993c06a68c9e4c31055a78f9d6
aarondevon <me@aarondevon.com>
2017-02-13 08:29:35 -0600

    Ask for user full name then greet them with it

---
+++ chapter-5/greeting.rb
@@ -0,0 +1,8 @@
+puts "Please enter your first name"
+first_name = gets.chomp
+puts "Please enter your middle name"
+middle_name = gets.chomp
+puts "please enter your last name"
+last_name = gets.chomp
+
+puts "It is very nice to meet you #{first_name} #{middle_name} #{last_name}!"
