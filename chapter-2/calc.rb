ce758373eaf3571d95c29a3203cd08bbe06455c1
aarondevon <me@aarondevon.com>
2017-02-13 08:32:09 -0600

    Calculate area of triangel, Future Value, hours in a year, minutes in a decade, my age in seconds, author's age in years

--- chapter-2/calc.rb
+++ chapter-2/calc.rb
@@ -1,1 +1,22 @@
 puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+
+# area of a triangle
+puts 12 * 4 / 2
+
+# Future Value
+puts 1200 * (1 + 0.065) ** 5
+
+# hours in a year
+puts "There are #{24 * 365} hours in a year."
+
+# minutes in a decade
+puts "There are #{60 * 24 * 365 * 10} minutes in a decade."
+
+# my age in seconds (February 11, 2017)
+puts "I am  about #{60 * 60 * 24 * ((365 * 31) + 286)} seconds old!"
+
+# calculate author's age
+puts "The author is about #{1160000000 / 60 / 60 / 24 / 365} years old."
