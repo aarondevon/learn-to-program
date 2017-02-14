94d92f631168db004fec69c6a888e39d2240f64d
aarondevon <me@aarondevon.com>
2017-02-14 09:35:19 -0600

    find number of letters in your name

---
+++ chapter-6/name-length.rb
@@ -0,0 +1,12 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your middle name?"
+middle_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+full_name = first_name + middle_name + last_name
+
+puts "Did you know that your name has #{full_name.length.to_i} letters?"
