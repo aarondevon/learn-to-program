67e4089d2f2783796624f14af067b9e47bc8a66b
aarondevon <me@aarondevon.com>
2017-02-14 11:52:11 -0600

    create variables for table of contents

---
+++ chapter-6/table-of-contents.rb
@@ -0,0 +1,10 @@
+line_width = 100
+
+contents = "Table of Contents"
+chapter_1 = "Chapter 1:  Getting Started"
+chapter_2 = "Chapter 2:  Numbers"
+chapter_3 = "Chapter 3:  Letters"
+
+chapter_1_page_start = "page  1"
+chapter_2_page_start = "page  9"
+chapter_3_page_start = "page 13"
