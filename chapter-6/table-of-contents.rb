6529c28e74e538271bf12b644c394ae7145176ec
aarondevon <me@aarondevon.com>
2017-02-14 12:20:46 -0600

    update formating

--- chapter-6/table-of-contents.rb
+++ chapter-6/table-of-contents.rb
@@ -11,7 +11,7 @@ chapter_3_page_start = "page 13"
 
 puts contents.center(line_width)
 puts
-puts chapter_1.ljust(line_width/2) + chapter_1_page_start.rjust(line_width/2)
-# changed argument to 27 because it need to more padding spots than chapter 1
-puts chapter_2.ljust(line_width/2) + chapter_2_page_start.rjust(27)
-puts chapter_3.ljust(line_width/2) + chapter_3_page_start.rjust(27)
+puts chapter_1.ljust(27) + chapter_1_page_start.rjust(23)
+# changed ljust argument to 27 and rjust to 23 because chapter_1 containts 27 charcters
+puts chapter_2.ljust(27) + chapter_2_page_start.rjust(23)
+puts chapter_3.ljust(27) + chapter_3_page_start.rjust(23)
