68dd4f4af23511d4ae64e481c153d7ca1bbf9ec2
aarondevon <me@aarondevon.com>
2017-02-14 18:47:48 -0600

    finish grandma app

---
+++ chapter-6/deaf-grandma.rb
@@ -0,0 +1,13 @@
+# Whatever is typed in grandma should respond, "HUH?! SPEAK UP, SONNY!"
+# Unless it is all caps then grandma should say, "NO, NOT SINCE 1938!"
+
+while true
+  puts "Say something to grandma."
+  talk_to_grandma = gets.chomp
+  if talk_to_grandma != talk_to_grandma.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "NO, NOT SINCE 1938!"
+    break
+  end
+end
