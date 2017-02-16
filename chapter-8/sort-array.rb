8847e7d74cd2f18fa332d1fabf7c1e1a6ff158c6
aarondevon <me@aarondevon.com>
2017-02-16 11:01:42 -0600

    sorts and quits on enter

--- chapter-8/sort-array.rb
+++ chapter-8/sort-array.rb
@@ -1,3 +1,13 @@
 # allow user to enter as many words as they want (one word per line)
 # sort array
 # quit program when user hits "enter" on empty line
+
+word_array = []
+word = "hi"
+
+while !(word.empty?)
+  word = gets.chomp
+  word_array.push word
+end
+
+puts word_array.sort
