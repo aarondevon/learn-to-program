1523a48013a6cda6ab480b5dc27a1e2eb37edf42
aarondevon <me@aarondevon.com>
2017-02-16 14:56:29 -0600

    Use array to print table of contents

--- chapter-8/table-of-contents-revisited.rb
+++ chapter-8/table-of-contents-revisited.rb
@@ -1,1 +1,17 @@
 # rewrite table of contents from chapter 6 using an array
+
+# set line width
+line_width = 50
+
+# create array
+book_array = ["Chapter 1:  Getting Started", "Chapter 2:  Numbers", "Chapter 3:  Letters", "page  1", "page  9", "page 13"]
+
+puts "Table of Contents".center(line_width)
+
+index = 0
+while index <= book_array.length
+  if index <= 3
+    puts book_array[index].ljust(27) + book_array[(index + 3)].rjust(23)
+  end
+  index += 1
+end
