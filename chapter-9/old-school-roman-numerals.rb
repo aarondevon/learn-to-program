258b396e56e9434de35ed606f50384a68c160e25
aarondevon <me@aarondevon.com>
2017-02-20 08:07:13 -0600

    calculate C L X V and I

--- chapter-9/old-school-roman-numerals.rb
+++ chapter-9/old-school-roman-numerals.rb
@@ -4,7 +4,22 @@
 def old_school_roman_numerial number
   roman = ''
 
+  # 1000
   roman << "M" * (number / 1000)
+  # 500
   roman << "D" * (number % 1000 / 500)
-  
+  # 100
+  roman << "C" * (number % 1000 % 500 / 100)
+  # 50
+  roman << "L" * (number % 1000 % 500 % 100 / 50)
+  # 10
+  roman << "X" * (number % 1000 % 500 % 100 % 50 / 10)
+  # 5
+  roman << "V" * (number % 1000 % 500 % 100 % 50 % 10 / 5)
+  # 1
+  roman << "I" * (number % 1000 % 500 % 100 % 50 % 10 % 5 / 1)
+
+  puts roman
 end
+
+old_school_roman_numerial 6
