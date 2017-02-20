4d5e9a089b48a7d843b45d3e9f5e693849a4ba5b
aarondevon <me@aarondevon.com>
2017-02-20 07:55:45 -0600

    calculate number of M's

--- chapter-9/old-school-roman-numerals.rb
+++ chapter-9/old-school-roman-numerals.rb
@@ -1,2 +1,9 @@
 # write a method that when passed a number between 1 and 3000 returns a string
 # containing the proper old-school roman numerals. 4 would be IIII and 9 would VIIII.
+
+def old_school_roman_numerial number
+  roman = ''
+
+  roman << "M" * (number / 1000)
+
+end
