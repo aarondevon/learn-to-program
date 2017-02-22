538f2634e9823ea87b0493fc4da9b196425493a0
aarondevon <me@aarondevon.com>
2017-02-22 09:10:57 -0600

    create method that sorts alphabetically and takes into account capitalization

--- chapter-10/dictionary-sort.rb
+++ chapter-10/dictionary-sort.rb
@@ -1,1 +1,27 @@
 # create a method that sorts alphabetically and takes into account capitalization
+
+words = ["fish", "Minnesota", "Abigail", "pie", "zoo", "apple"]
+
+def dictionary_sort unsorted_array
+  recursive_sort unsorted_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  sorted_word = unsorted_array[0]
+
+  unsorted_array.each do |word|
+    if word.downcase < sorted_word.downcase
+      sorted_word = word
+    end
+  end
+  sorted_array << sorted_word
+  unsorted_array.delete(sorted_word)
+
+  recursive_sort unsorted_array, sorted_array
+end
+
+puts dictionary_sort words
