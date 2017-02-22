e1f84109365cf3bb1bb093c1868ac35b2bf82d2e
aarondevon <me@aarondevon.com>
2017-02-22 07:37:35 -0600

    recursive sort method

--- chapter-10/recursive-sort.rb
+++ chapter-10/recursive-sort.rb
@@ -1,1 +1,31 @@
 # create a recursive sort program
+
+words = ["fish", "snacks", "dog", "pie", "cat", "apple"]
+sorted = []
+# def sort unsorted_array, sorted_array
+#   recursive_sort unsorted_array, []
+#
+# end
+
+def recursive_sort unsorted_array, sorted_array
+  # word to compare other words against
+  sorted_word = unsorted_array[0]
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  unsorted_array.each do |word|
+    if word < sorted_word
+      sorted_word = word
+    end
+  end
+  sorted_array << sorted_word
+  unsorted_array.delete(sorted_word)
+  recursive_sort unsorted_array, sorted_array
+
+end
+
+recursive_sort words, sorted
+
+
+p sorted
