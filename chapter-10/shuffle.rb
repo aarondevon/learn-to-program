369662295af7607c50aa821220dc2f4dc35beaf9
aarondevon <me@aarondevon.com>
2017-02-22 08:26:23 -0600

    test shuffle idea using rand

--- chapter-10/shuffle.rb
+++ chapter-10/shuffle.rb
@@ -1,1 +1,16 @@
 # create a program that shuffles an array and returns a shuffled version
+
+words = ["fish", "snacks", "dog", "pie", "cat", "apple"]
+
+shuffled_words = []
+
+length = words.length
+counter = 0
+while counter < length
+  word = words[rand(0..(words.length - 1))]
+  shuffled_words << word
+  words.delete(word)
+  counter += 1
+end
+
+puts shuffled_words
